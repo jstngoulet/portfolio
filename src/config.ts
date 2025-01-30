0e90c87e7cd4d7fcbceb5b43a0959be178c3c5f7
jstngoulet <jstngoulet@me.com>
2025-01-30 13:27:51 -0800

    Updated vars

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 
 
 //  Firebase
-export const GOOGLE_API_KEY = process.env.VITE_GOOGLE_API_KEY || 'AIzaSyB59OMhL-7cUxlA3-_ZH2eX3UCNYWNYsg4';
-export const GOOGLE_AUTH_DOMAIN = process.env.VITE_GOOGLE_AUTH_DOMAIN || 'tizzle-portfolio.firebaseapp.com';
-export const GOOGLE_PROJECT_ID = process.env.VITE_GOOGLE_PROJECT_ID || 'tizzle-portfolio';
-export const GOOGLE_STORAGE_BUCKET  = process.env.VITE_GOOGLE_STORAGE_BUCKET || "tizzle-portfolio.firebasestorage.app";
-export const GOOGLE_SENDER_ID = process.env.VITE_GOOGLE_SENDER_ID || "93800741164";
-export const GOOGLE_APP_ID = process.env.VITE_GOOGLE_APP_ID || '1:93800741164:web:9fc1caeea5762339a99489';
-export const GOOGLE_MEASUREMENT_ID = process.env.VITE_GOOGLE_MEASUREMENT_ID || 'G-RV6S9FQZVL';
+export const GOOGLE_API_KEY = 'AIzaSyB59OMhL-7cUxlA3-_ZH2eX3UCNYWNYsg4';
+export const GOOGLE_AUTH_DOMAIN = 'tizzle-portfolio.firebaseapp.com';
+export const GOOGLE_PROJECT_ID = 'tizzle-portfolio';
+export const GOOGLE_STORAGE_BUCKET = "tizzle-portfolio.firebasestorage.app";
+export const GOOGLE_SENDER_ID = "93800741164";
+export const GOOGLE_APP_ID ='1:93800741164:web:9fc1caeea5762339a99489';
+export const GOOGLE_MEASUREMENT_ID = 'G-RV6S9FQZVL';
