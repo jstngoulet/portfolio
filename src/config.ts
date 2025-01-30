a1c07f7e943ca98bbb14820d27cac6a99a96339c
jstngoulet <jstngoulet@me.com>
2025-01-30 13:24:04 -0800

    Adding missing semicolons

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 
 
 //  Firebase
-export const GOOGLE_API_KEY         = process.env.VITE_GOOGLE_API_KEY
-export const GOOGLE_AUTH_DOMAIN     = process.env.VITE_GOOGLE_AUTH_DOMAIN
+export const GOOGLE_API_KEY         = process.env.VITE_GOOGLE_API_KEY;
+export const GOOGLE_AUTH_DOMAIN     = process.env.VITE_GOOGLE_AUTH_DOMAIN;
 export const GOOGLE_PROJECT_ID      = process.env.VITE_GOOGLE_PROJECT_ID;
-export const GOOGLE_STORAGE_BUCKET  = process.env.VITE_GOOGLE_STORAGE_BUCKET
-export const GOOGLE_SENDER_ID       = process.env.VITE_GOOGLE_SENDER_ID
-export const GOOGLE_APP_ID          = process.env.VITE_GOOGLE_APP_ID
-export const GOOGLE_MEASUREMENT_ID  = process.env.VITE_GOOGLE_MEASUREMENT_ID
+export const GOOGLE_STORAGE_BUCKET  = process.env.VITE_GOOGLE_STORAGE_BUCKET;
+export const GOOGLE_SENDER_ID       = process.env.VITE_GOOGLE_SENDER_ID;
+export const GOOGLE_APP_ID          = process.env.VITE_GOOGLE_APP_ID;
+export const GOOGLE_MEASUREMENT_ID  = process.env.VITE_GOOGLE_MEASUREMENT_ID;
