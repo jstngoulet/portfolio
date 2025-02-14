a223021918ebe51b06f5c527243b491dfcedafd4
jstngoulet <jstngoulet@me.com>
2025-02-14 13:45:51 -0800

    reordered recommendations

--- src/_data/recommendations.tsx
+++ src/_data/recommendations.tsx
@@ -5,6 +5,96 @@ import RecommendationProps from '@/types/RecommendationProps'
 
 export const recommendations: RecommendationProps[] = [
   {
+    userPhotoUrl: `https://media.licdn.com/dms/image/v2/D5603AQFJAcAq3cea6Q/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1710328511266?e=1744243200&v=beta&t=xs81z0iVXpRkGsntSGC0aISxyI6DJNx9KxWnOPH1Jls`,
+    userName: `AJ Lee`,
+    userRole: `Interim CEO @ Getaround`,
+    userPosition: `Senior to me, but didn't manage me directly`,
+    reviewText: (
+      <span>
+        <p>
+          I had the pleasure of working with Justin at HyreCar and Getaround for nearly 3 years. He
+          was responsible for the HyreCar app and in that critical role, demonstrated an exceptional
+          grasp of mobile development best practices and a deep commitment to delivering
+          high-quality, user-centric experiences.
+        </p>
+        <p>
+          Justin's an excellent out-of-the-box thinker who consistently brought creative solutions
+          to challenging engineering problems -during my first week at HyreCar, we worked together
+          to dive into the numbers and the code to identify a major bug that was impacting
+          conversion. He then took ownership of the issue and fixed it within a day -I've been
+          impressed with his output ever since.
+        </p>
+        <p>
+          Another rare trait: he brings a uniquely customer-centric approach to his role that
+          ensured we delivered products that met the needs of our users and drove business success;
+          more than just execution and writing of code, he demonstrated a true care for the customer
+          (on both sides of the marketplace) and was a vocal advocate for delivering exceptional
+          customer experiences.
+        </p>{' '}
+      </span>
+    ),
+    dateGiven: `January 3, 2025`,
+    profileUrl: `https://www.linkedin.com/in/asjlee?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recommendations_details%3Bc2ub7zejQcqAAhzwj%2BlIvQ%3D%3D`
+  },
+  {
+    userPhotoUrl: `https://media.licdn.com/dms/image/v2/D5603AQEoyUXV3WiA0g/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1724298436941?e=1744848000&v=beta&t=G9DhM5vncUsaoSwhDgt9AhaqieeGh0IV_CWdyJgo2c4`,
+    userName: `Amit Gangwar`,
+    userRole: `VP Engineering`,
+    userPosition: `Managed me Directly`,
+    reviewText: (
+      <span>
+        <p>
+          I had the privilege of managing Justin directly at HyreCar and was consistently impressed
+          by his technical expertise and leadership. As Engineering Manager for the Driver Mobile
+          Applications team, Justin successfully led a cross-functional team of iOS, Android, Web,
+          and Backend engineers, delivering impactful features and collaborating with stakeholders
+          to develop innovative products. Justin's ability to combine technical skills with
+          strategic thinking makes him a standout professional. His leadership, collaborative
+          mindset, and focus on delivering value would make him an asset to any organization. I
+          wholeheartedly recommend him for roles where innovation and execution are paramount.
+        </p>
+      </span>
+    ),
+    dateGiven: `February 10, 2025`,
+    profileUrl: `https://www.linkedin.com/in/agangwar?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recommendations_details%3BbqlSVT5xQOuFUNPOVsOd2g%3D%3D`
+  },
+  {
+    userPhotoUrl: `https://media.licdn.com/dms/image/v2/C4E03AQHqq6yfgiIHIQ/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1539317025584?e=1744243200&v=beta&t=XWEv3U1hIGSlcJeX-f2G5tLjQmSnF8k3z6MAqOhFqq8`,
+    userName: `Bruce Larsen`,
+    userRole: `Lead Architect`,
+    userPosition: `Managed me directly`,
+    reviewText: (
+      <span>
+        <p>
+          I highly recommend Tizzle as an exceptional engineering leader. During our time working
+          together, Tizzle proved to be an invaluable asset to the team. His measured approach to
+          problem-solving brought valuable insights, and he consistently showcased his mastery of
+          product analytics. Tizzle was never short of ideas and always went the extra mile to
+          ensure success.
+        </p>
+        <p>
+          As a leader, I had the privilege of witnessing Tizzle's remarkable qualities firsthand. He
+          earned the respect and admiration of his team members through his patient and
+          collaborative approach. Tizzle's ability to work closely with each team member, offering
+          guidance and support, made a lasting impact on our collective success.
+        </p>
+        <p>
+          Furthermore, Tizzle made significant contributions to our "Domain Driven Design" and
+          "CQRS" initiative, playing a crucial role in their overall success. His expertise and
+          dedication were instrumental in implementing these complex concepts and driving positive
+          outcomes for the organization.
+        </p>
+        <p>
+          Tizzle's dedication, expertise, and exceptional leadership make him an outstanding
+          professional. I wholeheartedly recommend him for any engineering leadership role,
+          confident that he will continue to excel and drive remarkable results.
+        </p>
+      </span>
+    ),
+    dateGiven: `June 1, 2023`,
+    profileUrl: `https://www.linkedin.com/in/brace-larsen-engineer?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recommendations_details%3Bc2ub7zejQcqAAhzwj%2BlIvQ%3D%3D`
+  },
+  {
     userPhotoUrl: `https://media.licdn.com/dms/image/v2/D5603AQGtvTMMefYpVw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1683653352372?e=1744848000&v=beta&t=Q8RNFHG_NkXNFG6L_4rZr284b83wxZ_N0pyJbNultjE`,
     userName: `Nikita Urunkar`,
     userRole: `Business Intelligence Manager`,
@@ -80,28 +170,6 @@ export const recommendations: RecommendationProps[] = [
     ),
     dateGiven: `February 12, 2025`,
     profileUrl: `https://www.linkedin.com/in/priya-gandhi-99078797?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recommendations_details%3BR3%2F%2BIIugRSCG%2B0xEMtOzKQ%3D%3D`
-  },
-  {
-    userPhotoUrl: `https://media.licdn.com/dms/image/v2/D5603AQEoyUXV3WiA0g/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1724298436941?e=1744848000&v=beta&t=G9DhM5vncUsaoSwhDgt9AhaqieeGh0IV_CWdyJgo2c4`,
-    userName: `Amit Gangwar`,
-    userRole: `VP Engineering`,
-    userPosition: `Managed me Directly`,
-    reviewText: (
-      <span>
-        <p>
-          I had the privilege of managing Justin directly at HyreCar and was consistently impressed
-          by his technical expertise and leadership. As Engineering Manager for the Driver Mobile
-          Applications team, Justin successfully led a cross-functional team of iOS, Android, Web,
-          and Backend engineers, delivering impactful features and collaborating with stakeholders
-          to develop innovative products. Justin's ability to combine technical skills with
-          strategic thinking makes him a standout professional. His leadership, collaborative
-          mindset, and focus on delivering value would make him an asset to any organization. I
-          wholeheartedly recommend him for roles where innovation and execution are paramount.
-        </p>
-      </span>
-    ),
-    dateGiven: `February 10, 2025`,
-    profileUrl: `https://www.linkedin.com/in/agangwar?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recommendations_details%3BbqlSVT5xQOuFUNPOVsOd2g%3D%3D`
   },
   {
     userPhotoUrl: `https://media.licdn.com/dms/image/v2/C4D03AQFRNZ_9qLPUJw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1516767933955?e=1744848000&v=beta&t=HdTY1NyP8adKrvrhsilnb2PUSG9FMYCgEyOgFDbukE0`,
@@ -183,38 +251,6 @@ export const recommendations: RecommendationProps[] = [
     profileUrl: `https://www.linkedin.com/in/mohsen-masoudi?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recommendations_details%3BqsyzQdq5T2i0NNo%2B%2BVxahg%3D%3D`
   },
   {
-    userPhotoUrl: `https://media.licdn.com/dms/image/v2/D5603AQFJAcAq3cea6Q/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1710328511266?e=1744243200&v=beta&t=xs81z0iVXpRkGsntSGC0aISxyI6DJNx9KxWnOPH1Jls`,
-    userName: `AJ Lee`,
-    userRole: `Interim CEO @ Getaround`,
-    userPosition: `Senior to me, but didn't manage me directly`,
-    reviewText: (
-      <span>
-        <p>
-          I had the pleasure of working with Justin at HyreCar and Getaround for nearly 3 years. He
-          was responsible for the HyreCar app and in that critical role, demonstrated an exceptional
-          grasp of mobile development best practices and a deep commitment to delivering
-          high-quality, user-centric experiences.
-        </p>
-        <p>
-          Justin's an excellent out-of-the-box thinker who consistently brought creative solutions
-          to challenging engineering problems -during my first week at HyreCar, we worked together
-          to dive into the numbers and the code to identify a major bug that was impacting
-          conversion. He then took ownership of the issue and fixed it within a day -I've been
-          impressed with his output ever since.
-        </p>
-        <p>
-          Another rare trait: he brings a uniquely customer-centric approach to his role that
-          ensured we delivered products that met the needs of our users and drove business success;
-          more than just execution and writing of code, he demonstrated a true care for the customer
-          (on both sides of the marketplace) and was a vocal advocate for delivering exceptional
-          customer experiences.
-        </p>{' '}
-      </span>
-    ),
-    dateGiven: `January 3, 2025`,
-    profileUrl: `https://www.linkedin.com/in/asjlee?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recommendations_details%3Bc2ub7zejQcqAAhzwj%2BlIvQ%3D%3D`
-  },
-  {
     userPhotoUrl: `https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.rcmXeqCUOiCg54dfU4v9tgHaHa%26pid%3DApi&f=1&ipt=6e8ab046ba2a9b0e17fe064622e29e22a4cb3fa3aaa11ecc6567e7a4d1eb457b&ipo=images`,
     userName: `Tanvir Sharif`,
     userRole: `Lead Data Engineer`,
@@ -271,42 +307,6 @@ export const recommendations: RecommendationProps[] = [
     dateGiven: `June 1, 2023`,
     profileUrl: `https://www.linkedin.com/in/raymond-abarca-b50824238?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recommendations_details%3Bc2ub7zejQcqAAhzwj%2BlIvQ%3D%3D`
   },
-  {
-    userPhotoUrl: `https://media.licdn.com/dms/image/v2/C4E03AQHqq6yfgiIHIQ/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1539317025584?e=1744243200&v=beta&t=XWEv3U1hIGSlcJeX-f2G5tLjQmSnF8k3z6MAqOhFqq8`,
-    userName: `Bruce Larsen`,
-    userRole: `Lead Architect`,
-    userPosition: `Managed me directly`,
-    reviewText: (
-      <span>
-        <p>
-          I highly recommend Tizzle as an exceptional engineering leader. During our time working
-          together, Tizzle proved to be an invaluable asset to the team. His measured approach to
-          problem-solving brought valuable insights, and he consistently showcased his mastery of
-          product analytics. Tizzle was never short of ideas and always went the extra mile to
-          ensure success.
-        </p>
-        <p>
-          As a leader, I had the privilege of witnessing Tizzle's remarkable qualities firsthand. He
-          earned the respect and admiration of his team members through his patient and
-          collaborative approach. Tizzle's ability to work closely with each team member, offering
-          guidance and support, made a lasting impact on our collective success.
-        </p>
-        <p>
-          Furthermore, Tizzle made significant contributions to our "Domain Driven Design" and
-          "CQRS" initiative, playing a crucial role in their overall success. His expertise and
-          dedication were instrumental in implementing these complex concepts and driving positive
-          outcomes for the organization.
-        </p>
-        <p>
-          Tizzle's dedication, expertise, and exceptional leadership make him an outstanding
-          professional. I wholeheartedly recommend him for any engineering leadership role,
-          confident that he will continue to excel and drive remarkable results.
-        </p>
-      </span>
-    ),
-    dateGiven: `June 1, 2023`,
-    profileUrl: `https://www.linkedin.com/in/brace-larsen-engineer?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recommendations_details%3Bc2ub7zejQcqAAhzwj%2BlIvQ%3D%3D`
-  }
   //   {
   //     userPhotoUrl: ``,
   //     userName: ``,
