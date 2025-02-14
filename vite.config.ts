d8ec7778474ca2b4b81cfff80fb04a38ae59984f
jstngoulet <jstngoulet@me.com>
2025-02-14 13:51:17 -0800

    readded php

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import usePHP from 'vite-plugin-php'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
+    usePHP({
+       binary: '/opt/lampp/bin/php-8.1.10', 
+       entry: ['php/*.php'] 
+      }
+    )
   ],
   resolve: {
     alias: {
